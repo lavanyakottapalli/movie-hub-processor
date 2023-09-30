1669678149055d2fc7df6ae65e42210e88208adb
lavayakottapalli <lavanyakottapalli96@gmail.com>
2023-09-30 16:18:18 -0700

    Add processor

---
+++ src/models/MovieId.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="MovieId.cs" company="Lavanya.">
+// </copyright>
+
+using System.Text.Json.Serialization;
+
+namespace Microsoft.Movie.Hub.Processor.Models
+{
+    /// <summary>
+    /// Movie identifier
+    /// </summary>
+    public class MovieId
+    {
+        /// <summary>
+        /// Gets or sets the movide id.
+        /// </summary>
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+    }
+}
